a5158482fa9c3431cad52cbea4e6673ada8c50c4
Ozoda Shakhobiddinova <2110074@newuzbekistanuniversity.uz>
2022-11-10 18:45:52 +0500

    add

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const formEl = document.querySelector(".form");
 const inputEl = document.querySelector(".input");
 const listEl = document.querySelector(".list");
 const selectEl = document.querySelector(".type_select");
+const yearInput = document.querySelector(".year_input");
 
 function renderMovies(element) {
   const movieFragment = document.createDocumentFragment();
@@ -17,16 +18,19 @@ function renderMovies(element) {
     listTitle.textContent = `Title: ${item.Title}`;
     const listType = document.createElement("p");
     listType.textContent = `Type : ${item.Type}`;
+    const listYear = document.createElement("p");
+    listYear.textContent = `Year: ${item.Year}`;
     listItem.appendChild(listImg);
     listItem.appendChild(listTitle);
     listItem.appendChild(listType);
+    listItem.appendChild(listYear);
     movieFragment.appendChild(listItem);
   });
   listEl.appendChild(movieFragment);
 }
-function renderUrl(inputValue, selectValues) {
+function renderUrl(inputValue, selectValues, yearValue) {
   fetch(
-    `http://www.omdbapi.com/?apikey=f504a03b&s=${inputValue}&type=${selectValues}`
+    `http://www.omdbapi.com/?apikey=f504a03b&s=${inputValue}&type=${selectValues}&y=${yearValue}`
   )
     .then((response) => response.json())
     .then((data) => {
@@ -38,5 +42,6 @@ formEl.addEventListener("submit", function (evt) {
   listEl.innerHTML = "";
   const inputValue = inputEl.value.trim();
   const selectValues = selectEl.value;
-  renderUrl(inputValue, selectValues);
+  const yearValue = yearInput.value;
+  renderUrl(inputValue, selectValues, yearValue);
 });
